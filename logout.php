d42f4a23e7951607f70955f632c8bd406ba01400
blen-assefa <91486202+blen-assefa@users.noreply.github.com>
2021-11-04 21:32:27 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
 <?php
-  session_start(); /* Starts the session */
-  session_destroy(); /* Destroy started session */
 
-  header("location:login.php");  /* Redirect to login page */
-  exit;
+// Initialize the session
+session_start();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
