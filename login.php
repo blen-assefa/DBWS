1e3f27c374e7e7b5a86851977cdab819ccd86998
blen-assefa <91486202+blen-assefa@users.noreply.github.com>
2021-11-04 21:29:54 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,74 +1,126 @@
 <?php
-require_once ('config.php'); // For storing username and password.
-
-session_start();
-?>
-
-<!-- HTML code for Bootstrap framework and form design -->
-
+   // Initialize the session
+   session_start();
+    
+   // Check if the user is already logged in, if yes then redirect him to welcome page
+   if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+       header("location: welcome.php");
+       exit;
+   }
+    
+   // Include config file
+   require_once "config.php";
+    
+   // Define variables and initialize with empty values
+   $username = $password = "";
+   $username_err = $password_err = $login_err = "";
+    
+   // Processing form data when form is submitted
+   if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+       // Check if username is empty
+       if(empty(trim($_POST["username"]))){
+           $username_err = "Please enter username.";
+       } else{
+           $username = trim($_POST["username"]);
+       }
+       
+       // Check if password is empty
+       if(empty(trim($_POST["password"]))){
+           $password_err = "Please enter your password.";
+       } else{
+           $password = trim($_POST["password"]);
+       }
+       
+       // Validate credentials
+       if(empty($username_err) && empty($password_err)){
+           // Prepare a select statement
+           $sql = "SELECT id, username, password FROM users WHERE username = :username";
+           
+           if($stmt = $pdo->prepare($sql)){
+               // Bind variables to the prepared statement as parameters
+               $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+               
+               // Set parameters
+               $param_username = trim($_POST["username"]);
+               
+               // Attempt to execute the prepared statement
+               if($stmt->execute()){
+                   // Check if username exists, if yes then verify password
+                   if($stmt->rowCount() == 1){
+                       if($row = $stmt->fetch()){
+                           $id = $row["id"];
+                           $username = $row["username"];
+                           $hashed_password = $row["password"];
+                           if(password_verify($password, $hashed_password)){
+                               // Password is correct, so start a new session
+                               session_start();
+                               
+                               // Store data in session variables
+                               $_SESSION["loggedin"] = true;
+                               $_SESSION["id"] = $id;
+                               $_SESSION["username"] = $username;                            
+                               
+                               // Redirect user to welcome page
+                               header("location: welcome.php");
+                           } else{
+                               // Password is not valid, display a generic error message
+                               $login_err = "Invalid username or password.";
+                           }
+                       }
+                   } else{
+                       // Username doesn't exist, display a generic error message
+                       $login_err = "Invalid username or password.";
+                   }
+               } else{
+                   echo "Oops! Something went wrong. Please try again later.";
+               }
+   
+               // Close statement
+               unset($stmt);
+           }
+       }
+       
+       // Close connection
+       unset($pdo);
+   }
+   ?>
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/signin.css">
-    <title>Sign in</title>
-</head>
-<body>
-<div class="container">
-    <form action="" method="post" name="Login_Form" class="form-signin">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputUsername" class="sr-only">Username</label>
-        <input name="Username" type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input name="Password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
-        <button name="Submit" value="Login" class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-
-        <?php
-
-        /* Check if login form has been submitted */
-        if(isset($_POST['Submit'])){
-
-            // Rudimentary hash check
-            $result = password_verify($_POST['Password'], $Password);
-
-            /* Check if form's username and password matches */
-            if( ($_POST['Username'] == $Username) && ($result === true) ) {
-
-                /* Success: Set session variables and redirect to protected page */
-                $_SESSION['Username'] = $Username;
-
-                $_SESSION['Active'] = true;
-                header("location:index.php");
-                exit;
-
-            } else {
-                ?>
-                <!-- Show an error alert -->
-                &nbsp;
-                <div class="alert alert-danger alert-dismissible" role="alert">
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <strong>Warning!</strong> Incorrect information.
-                </div>
-                <?php
-            }
-        }
-        ?>
-
-    </form>
-</div>
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-</body>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <title>Login</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <style>
+         body{ font: 14px sans-serif; }
+         .wrapper{ width: 360px; padding: 20px; }
+      </style>
+   </head>
+   <body>
+      <div class="wrapper">
+         <h2>Login</h2>
+         <p>Please fill in your credentials to login.</p>
+         <?php 
+            if(!empty($login_err)){
+                echo '<div class="alert alert-danger">' . $login_err . '</div>';
+            }        
+            ?>
+         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+               <label>Username</label>
+               <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+               <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>
+            <div class="form-group">
+               <label>Password</label>
+               <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+               <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+               <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+         </form>
+      </div>
+   </body>
 </html>
