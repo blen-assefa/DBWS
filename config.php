ea7be5cc9bf6f77dc9e14e3b036c4f9067b9d80d
blen-assefa <91486202+blen-assefa@users.noreply.github.com>
2021-11-04 21:34:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,16 @@
 <?php
-
-/* Define username and password */
-$Username = 'Steve';
-$Password = '$2y$10$1GmNO63bbKWpaPxcqLaLW.yVmvoxyOD9krWXxn2XAY.QSdbfcARDK';
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'group14');
+define('DB_PASSWORD', 'Y2YxSV');
+define('DB_NAME', 'group14');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
