b804e90346644fc4601b1a9e9e1bb83254d81fec
blen-assefa <91486202+blen-assefa@users.noreply.github.com>
2021-11-05 17:37:39 +0100

    Update register.php

--- register.php
+++ register.php
@@ -10,6 +10,12 @@ $email_err = $username_err = $password_err = $confirm_password_err = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a fucking username.";     
+    } else{
+        $username = trim($_POST["username"]);
+    }
  
     // Validate email
     if(empty(trim($_POST["email"]))){
@@ -119,7 +125,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-          
+           <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div> 
             <div class="form-group">
                 <label>Email</label>
                 <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
