4a586f5a3974e2c3020ff0abaddb20280a64de1f
blen-assefa <91486202+blen-assefa@users.noreply.github.com>
2021-11-05 16:42:06 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,126 +1,133 @@
 <?php
-   // Initialize the session
-   session_start();
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$email = $password = "";
+$email_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if email is empty
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter email.";
+    } else{
+        $email = trim($_POST["email"]);
+    }
     
-   // Check if the user is already logged in, if yes then redirect him to welcome page
-   if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-       header("location: index.php");
-       exit;
-   }
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
     
-   // Include config file
-   require_once "config.php";
+    // Validate credentials
+    if(empty($email_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT C.email, C.password FROM Customers WHERE C.email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = $email;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $customer_id, $email, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["customer_id"] = $customer_id;
+                            $_SESSION["email"] = $email;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: index.php");
+                        } else{
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid email or password.";
+                        }
+                    }
+                } else{
+                    // email doesn't exist, display a generic error message
+                    $login_err = "Email doesn't exit in the database";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
     
-   // Define variables and initialize with empty values
-   $email = $password = "";
-   $email_err = $password_err = $login_err = "";
-    
-   // Processing form data when form is submitted
-   if($_SERVER["REQUEST_METHOD"] == "POST"){
-    
-       // Check if username is empty
-       if(empty(trim($_POST["email"]))){
-           $email_err = "Please enter email.";
-       } else{
-           $email = trim($_POST["email"]);
-       }
-       
-       // Check if password is empty
-       if(empty(trim($_POST["password"]))){
-           $password_err = "Please enter your password.";
-       } else{
-           $password = trim($_POST["password"]);
-       }
-       
-       // Validate credentials
-       if(empty($email_err) && empty($password_err)){
-           // Prepare a select statement
-           $sql = "SELECT customer_id, email, password FROM Customers WHERE email = :email";
-           
-           if($stmt = $pdo->prepare($sql)){
-               // Bind variables to the prepared statement as parameters
-               $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
-               
-               // Set parameters
-               $param_email = trim($_POST["email"]);
-               
-               // Attempt to execute the prepared statement
-               if($stmt->execute()){
-                   // Check if username exists, if yes then verify password
-                   if($stmt->rowCount() == 1){
-                       if($row = $stmt->fetch()){
-                           $customer_id = $row["customer_id"];
-                           $email = $row["email"];
-                           $hashed_password = $row["password"];
-                           if(password_verify($password, $hashed_password)){
-                               // Password is correct, so start a new session
-                               session_start();
-                               
-                               // Store data in session variables
-                               $_SESSION["loggedin"] = true;
-                               $_SESSION["customer_id"] = $customer_id;
-                               $_SESSION["email"] = $email;                            
-                               
-                               // Redirect user to welcome page
-                               header("location: index.php");
-                           } else{
-                               // Password is not valid, display a generic error message
-                               $login_err = "Invalid email or password.";
-                           }
-                       }
-                   } else{
-                       // Username doesn't exist, display a generic error message
-                       $login_err = "Invalid email or password.";
-                   }
-               } else{
-                   echo "Oops! Something went wrong. Please try again later.";
-               }
-   
-               // Close statement
-               unset($stmt);
-           }
-       }
-       
-       // Close connection
-       unset($pdo);
-   }
-   ?>
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <meta charset="UTF-8">
-      <title>Login</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-      <style>
-         body{ font: 14px sans-serif; }
-         .wrapper{ width: 360px; padding: 20px; }
-      </style>
-   </head>
-   <body>
-      <div class="wrapper">
-         <h2>Login</h2>
-         <p>Please fill in your credentials to login.</p>
-         <?php 
-            if(!empty($login_err)){
-                echo '<div class="alert alert-danger">' . $login_err . '</div>';
-            }        
-            ?>
-         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="container h-100 d-flex justify-content-center">
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
-               <label>Email</label>
-               <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
-               <span class="invalid-feedback"><?php echo $email_err; ?></span>
+                <label>Email</label>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
-               <label>Password</label>
-               <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-               <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-               <input type="submit" class="btn btn-primary" value="Login">
+                <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-         </form>
-      </div>
-   </body>
+        </form>
+    </div>
+    </div>
+</body>
 </html>
