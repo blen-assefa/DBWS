0c35b469b3f09666252713a9fa07fe8476a23f85
blen-assefa <91486202+blen-assefa@users.noreply.github.com>
2021-11-17 21:05:39 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 // Database credentials. Assuming you are running MySQL
 // server with default setting (user 'root' with no password) 
 
@@ -14,6 +14,6 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-} */
+} 
 
 ?>
