321dacf6df9d666a96a7288f757f330daee42119
VolTer <mew.pur.pur@abv.bg>
2021-11-24 20:54:08 +0100

    Renamed  to  in config

--- config.php
+++ config.php
@@ -9,10 +9,10 @@ define('DB_PASSWORD', 'Y2YxSV');
 define('DB_NAME', 'group14');
  
 // Attempt to connect to MySQL database
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
-if($link === false){
+if($db === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 } 
 
