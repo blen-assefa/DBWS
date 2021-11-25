8e73c4a5266accbfe7ca1ca76579e67ce100d56d
VolTer <mew.pur.pur@abv.bg>
2021-11-25 19:40:40 +0100

    Added movie genres

--- admin/insertions/movie.php
+++ admin/insertions/movie.php
@@ -11,7 +11,15 @@ $duration = $_POST["duration"];
 $desc = mysqli_real_escape_string($_POST["description"]);
 $movie_id = generateKey($db);
 
-$sql = "INSERT INTO Movies VALUES ('$movie_id', '$name', '$desc', '$duration')";
+if (!isset($_POST['genre1'])) {
+  $genre = $_POST["genre2"];
+} elseif (!isset($_POST['genre2'])) {
+  $genre = $_POST["genre1"];
+} else {
+$genre = $_POST["genre1"] . "/" . $_POST["genre2"];
+}
+
+$sql = "INSERT INTO Movies VALUES ('$movie_id', '$genre', '$name', '$desc', '$duration')";
 echo "$sql</p>";
 
 mysqli_query($db, $sql) or die('Error querying database.');
