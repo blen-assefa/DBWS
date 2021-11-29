81d579236a79ef95783de766e6a421f73cb3a5d2
VolTer <mew.pur.pur@abv.bg>
2021-11-29 10:19:34 +0100

    Fixed movie insertion

--- admin/insertions/movie.php
+++ admin/insertions/movie.php
@@ -11,12 +11,52 @@ $duration = $_POST["duration"];
 $desc = mysqli_real_escape_string($db, $_POST["description"]);
 $movie_id = generateKey($db);
 
-if (!isset($_POST['genre1'])) {
-  $genre = $_POST["genre2"];
-} elseif (!isset($_POST['genre2'])) {
+// My hope is that this is so bad I'm never allowed to write PHP code again
+if (!isset($_POST['genre1']) && !isset($_POST['genre2']) && !isset($_POST['genre3']) && !isset($_POST['genre4'])) {
+  die('Invalid input.');
+}
+if (isset($_POST['genre1']) && !isset($_POST['genre2']) && !isset($_POST['genre3']) && !isset($_POST['genre4'])) {
   $genre = $_POST["genre1"];
+} elseif (!isset($_POST['genre1']) && isset($_POST['genre2']) && !isset($_POST['genre3']) && !isset($_POST['genre4'])) {
+  $genre = $_POST["genre2"];
+} elseif (!isset($_POST['genre1']) && !isset($_POST['genre2']) && isset($_POST['genre3']) && !isset($_POST['genre4'])) {
+  $genre = $_POST["genre3"];
+} elseif (!isset($_POST['genre1']) && !isset($_POST['genre2']) && !isset($_POST['genre3']) && isset($_POST['genre4'])) {
+  $genre = $_POST["genre4"];
 } else {
-$genre = $_POST["genre1"] . "/" . $_POST["genre2"];
+  $first = true;
+  if (isset($_POST['genre1'])) {
+    if ($first) {
+      $first = false;
+      $genre = $_POST['genre1'];
+    } else {
+      $genre = $genre . "/" . $_POST['genre1'];
+    }
+  }
+  if (isset($_POST['genre2'])) {
+    if ($first) {
+      $first = false;
+      $genre = $_POST['genre2'];
+    } else {
+      $genre = $genre . "/" . $_POST['genre2'];
+    }
+  }
+  if (isset($_POST['genre3'])) {
+    if ($first) {
+      $first = false;
+      $genre = $_POST['genre3'];
+    } else {
+      $genre = $genre . "/" . $_POST['genre3'];
+    }
+  }
+  if (isset($_POST['genre4'])) {
+    if ($first) {
+      $first = false;
+      $genre = $_POST['genre4'];
+    } else {
+      $genre = $genre . "/" . $_POST['genre4'];
+    }
+  }
 }
 
 $sql = "INSERT INTO Movies VALUES ('$movie_id', '$name', '$genre', '$desc', '$duration')";
