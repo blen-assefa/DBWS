1b6cf746d0ef49b69d6e37b7ba87b5c0b9dce038
Blen Assefa <blenassefa@MacBook-Pro-von-Blen.local>
2021-12-02 20:42:15 +0100

    Name changed

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+
 // Database credentials. Assuming you are running MySQL
 // server with default setting (user 'root' with no password) 
 
@@ -18,7 +18,7 @@ if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-*/
+
 
 
 
